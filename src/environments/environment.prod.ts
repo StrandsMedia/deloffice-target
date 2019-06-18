b6e666676ff91b53eb96ad958903e0e494c0a656
StrandsMedia <strands.media@gmail.com>
2019-06-18 14:36:51 +0400

    commit Nicolas 18.06.19 5

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'http://192.168.100.250/newapi/',
-  apiUrl2: 'http://192.168.100.250/newapi/',
-  imgUrl: 'http://192.168.100.250/imgdir/',
-  remoteURL: 'https://printnpack.mu/'
-};
