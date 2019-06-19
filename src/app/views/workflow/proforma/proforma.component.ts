40c3c596e6c5205f47ecd0dbf5093c1ae16c26d5
Coosoom <c.gootoo@deloffice.mu>
2019-06-19 11:52:35 +0400

    Commit Coosoom 19.06.19 2

--- src/app/views/workflow/proforma/proforma.component.ts
+++ src/app/views/workflow/proforma/proforma.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { AuthService } from '../../../common/services/auth.service';
+import { MaterialService } from '../../../common/services/material.service';
+import { WorkflowService } from '../../../common/services/workflow.service';
+import { QuestionControlService } from '../popup/utils/question-control.service';
+
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-proforma',
@@ -6,10 +15,108 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./proforma.component.scss']
 })
 export class ProformaComponent implements OnInit {
+  public dataSource$: Observable<any>;
+  public asc = true;
+  public loading = false;
+  clicked = 0;
+  sortKey: any;
+  public columns = [
+    'workflow_id',
+    'date',
+    'time',
+    'company_name',
+    'orderconfirm',
+    'profinprocess',
+    'proftobeamended',
+    'proftobecancelled',
+    'creditcontrol',
+    'profprocessed',
+    'duration'
+  ];
 
-  constructor() { }
+  public user: any;
 
-  ngOnInit() {
+  constructor(
+    private auth: AuthService,
+    private fb: FormBuilder,
+    private mdc: MaterialService,
+    private wf: WorkflowService,
+    private qcs: QuestionControlService,
+    private cdRef: ChangeDetectorRef
+  ) { 
+    this.auth.currentUser.subscribe(data => this.user = data);
+   }
+
+  ngOnInit(): void {
+    this.get();
+  }
+
+  trackByFn(index, item) {
+    return index;
+  }
+
+  get() {
+    this.loading = true;
+    return this.dataSource$ = this.wf.getWorkflow(1).pipe(
+      map((data: any) => {
+        return data.records;
+      }),
+      tap((data) => {
+        this.loading = false;
+      })
+    );
+  }
+
+  sortObs(key, direction) {
+    let dir;
+    direction ? dir = 'asc' : dir = 'desc';
+    return this.dataSource$ = this.wf.getWorkflow(1).pipe(
+      map((data: any) => data.records),
+      map((records) => {
+        return records.sort((a, b) => {
+          // tslint:disable-next-line:curly
+          if (a[key] > b[key]) return dir === 'asc' ? 1 : -1;
+          // tslint:disable-next-line:curly
+          if (a[key] < b[key]) return dir === 'asc' ? -1 : 1;
+          return 0;
+        });
+      })
+    );
+  }
+
+  sort($event) {
+    let sortKey = '';
+    switch (this.clicked) {
+      case 0:
+        sortKey = $event.target.getAttribute('data-name');
+        this.sortObs(sortKey, this.asc);
+        this.sortKey = sortKey;
+        this.clicked++;
+        break;
+      case 1:
+        sortKey = $event.target.getAttribute('data-name');
+        if (this.sortKey === sortKey) {
+          this.sortObs(sortKey, !this.asc);
+          this.clicked++;
+        } else {
+          this.sortObs(sortKey, this.asc);
+          this.clicked = 1;
+        }
+        this.sortKey = sortKey;
+        break;
+      case 2:
+        sortKey = $event.target.getAttribute('data-name');
+        if (this.sortKey === sortKey) {
+          this.get();
+          this.sortKey = '';
+          this.clicked = 0;
+        } else {
+          this.sortObs(sortKey, this.asc);
+          this.sortKey = sortKey;
+          this.clicked = 1;
+        }
+        break;
+    }
   }
 
 }
