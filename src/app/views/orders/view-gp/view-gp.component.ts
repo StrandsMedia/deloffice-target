c033a6db34b0ebb9a97c276858fe6198b70915f4
Coosoom <c.gootoo@deloffice.mu>
2019-06-25 16:39:50 +0400

    Commit Coosoom 25.06.19 1

--- src/app/views/orders/view-gp/view-gp.component.ts
+++ src/app/views/orders/view-gp/view-gp.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-// import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
-// import { Observable } from 'rxjs';
-// import { switchMap } from 'rxjs/operators';
+import { OrdersService } from '../../../common/services/orders.service';
+
+import { Observable, BehaviorSubject } from 'rxjs';
+import { tap, switchMap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-view-gp',
@@ -13,25 +15,66 @@ export class ViewGPComponent implements OnInit {
   // Hello Coosoom ^_^ !
 
   // You need to use an Observable to put the data inside it
-  // public invoice$: Observable<any>;
+  loading = false;
+  public invoice$: Observable<any>;
+
+  entries = [];
+  private _prodEntr$: BehaviorSubject<any[]> = new BehaviorSubject<any[]>([]);
+  public prodEnt$: Observable<any> = this._prodEntr$.asObservable();
+  public tempProd: any;
+
+  private _tempData: any;
 
   // You will also need columns for your table
-  // public columns = [];
+  public columns = [
+    'p_id',
+    'des',
+    'brand',
+    'qty',
+    'confirm',
+    'verify'
+  ];
 
   // public confirm: boolean;
   // public verify: boolean;
 
-  constructor() { }
+  constructor(
+    private _order: OrdersService,
+    private _route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.get();
   }
 
   // You need a getter function for the data
   get() {
     // Use the observable to assign it to the data from the service
+    this.invoice$ = this._route.params.pipe(
+      map(params => params.id),
+      switchMap(params => {
+        return this._order.getInvoice(+params);
+      }),
+      map((res: any) => res.records[0]),
+      tap((res) => {
+        this._tempData = res;
+        //this.searchProdForm.controls['cust_id'].setValue(res.cust_id);
+        this.entries = res.entries;
+
+        this.entries.forEach(entry => {
+          entry.status = 'existing';
+        });
+
+        this._prodEntr$.next(this.entries);
+
+        this.loading = !this.loading;
+      })
+    );
   }
 
-  //OK SIR :D
+  public trackByFn(index, item) {
+    return index;
+  }
 
   // public toggleConfirmMode() {
   //   this.confirm = !this.confirm;
